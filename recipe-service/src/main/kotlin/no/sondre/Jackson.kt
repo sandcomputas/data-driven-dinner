d493832dff2870d5ed2d9dbc58afc9d0b1cfd8ee
Sondre Andersen <sand@computas.com>
2025-01-02 20:11:06 +0100

    remove open api cutomization because it hides all nullable fields

--- recipe-service/src/main/kotlin/no/sondre/Jackson.kt
+++ recipe-service/src/main/kotlin/no/sondre/Jackson.kt
@@ -1,23 +1,26 @@
 package no.sondre
 
-import jakarta.annotation.Priority
-import jakarta.enterprise.context.ApplicationScoped
-import jakarta.enterprise.inject.Alternative
-import org.eclipse.microprofile.openapi.OASFilter
-import org.eclipse.microprofile.openapi.models.media.Schema
+//import jakarta.annotation.Priority
+//import jakarta.enterprise.context.ApplicationScoped
+//import jakarta.enterprise.inject.Alternative
+//import org.eclipse.microprofile.openapi.OASFilter
+//import org.eclipse.microprofile.openapi.models.media.Schema
+//
 
-
-@Alternative
-@Priority(1)
-@ApplicationScoped
-class CustomOpenAPIFilter : OASFilter {
-    override fun filterSchema(schema: Schema?): Schema? {
-        val properties = schema?.properties
-        if (properties != null) {
-            val toToKeep = schema.required.toList()
-            val mutableProperties = properties.toMutableMap()
-            schema.properties = mutableProperties.filter { it.key in toToKeep }
-        }
-        return schema
-    }
-}
+// TODO: this will hide all nullable fields, and that is not what we want
+//  what we want is to hide all PRIVATE fields, but I am not sure how to do it without
+//  adding annotations...
+//@Alternative
+//@Priority(1)
+//@ApplicationScoped
+//class CustomOpenAPIFilter : OASFilter {
+//    override fun filterSchema(schema: Schema?): Schema? {
+//        val properties = schema?.properties
+//        if (properties != null) {
+//            val toToKeep = schema.required.toList()
+//            val mutableProperties = properties.toMutableMap()
+//            schema.properties = mutableProperties.filter { it.key in toToKeep }
+//        }
+//        return schema
+//    }
+//}
--- recipe-service/src/main/kotlin/no/sondre/domain/Ingredient.kt
+++ recipe-service/src/main/kotlin/no/sondre/domain/Ingredient.kt
@@ -1,3 +1,5 @@
 package no.sondre.domain
 
-class Ingredient(var name: String) : Domain()
+class Ingredient(var name: String) : Domain() {
+    private val secretField: String = "hello"
+}
